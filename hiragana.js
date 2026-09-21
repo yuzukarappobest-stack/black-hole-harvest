ef117671574b00f125cb332eec98578845efa212
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-21 17:59:39 +0900

    Route hiragana rewards through game menu

--- hiragana.js
+++ hiragana.js
@@ -573,7 +573,7 @@ function showFeedback(text, type) {
 
 function showComplete() {
   issueRewardToken();
-  completePanel.classList.remove("hidden");
+  window.location.replace("game-menu.html");
 }
 
 function resetLesson() {

7bf3d421dbc90e9908809b4789d01a3b206a32dc
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-21 17:59:46 +0900

    Route hiragana-dakuon rewards through game menu

--- hiragana-dakuon.js
+++ hiragana-dakuon.js
@@ -538,7 +538,7 @@ function showFeedback(text, type) {
 
 function showComplete() {
   issueRewardToken();
-  completePanel.classList.remove("hidden");
+  window.location.replace("game-menu.html");
 }
 
 function resetLesson() {

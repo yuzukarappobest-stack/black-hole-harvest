66c702ad58354308d5433a1c194f6207d93f84bf
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-21 18:00:38 +0900

    Route hiragana-sentence-2 rewards through game menu

--- hiragana-sentence-2.js
+++ hiragana-sentence-2.js
@@ -229,7 +229,7 @@ function renderChoices(choices) {
 
 function showComplete() {
   issueRewardToken();
-  completePanel.classList.remove("hidden");
+  window.location.replace("game-menu.html");
 }
 
 function buildBalancedChoices(question) {

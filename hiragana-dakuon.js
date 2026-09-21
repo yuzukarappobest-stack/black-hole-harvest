afa07b6bbb6f15b7fcd820ff772c0647cf6f67f6
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-21 17:45:49 +0900

    Wire Kabuto Sumo reward in hiragana-dakuon

--- hiragana-dakuon.js
+++ hiragana-dakuon.js
@@ -23,6 +23,7 @@ const FROG_JUMP_GAME_ID = "frog-jump";
 const FRUIT_RUSH_GAME_ID = "fruit-rush";
 const SPACE_BLASTER_GAME_ID = "space-blaster";
 const KAIJU_DEFENSE_GAME_ID = "kaiju-defense";
+const KABUTO_SUMO_GAME_ID = "kabuto-sumo";
 
 const letterGroups = [
   ["が", "ぎ", "ぐ", "げ", "ご"],
@@ -210,6 +211,12 @@ const playFrogJumpButton = document.getElementById("playFrogJumpButton");
 const playFruitRushButton = document.getElementById("playFruitRushButton");
 const playSpaceBlasterButton = document.getElementById("playSpaceBlasterButton");
 const playKaijuDefenseButton = document.getElementById("playKaijuDefenseButton");
+const playKabutoSumoButton = document.getElementById("playKabutoSumoButton");
+playKabutoSumoButton.addEventListener("click", () => {
+  if (grantMiniGameAccess(KABUTO_SUMO_GAME_ID)) {
+    window.location.replace("kabuto-sumo/");
+  }
+});
 const stayButton = document.getElementById("stayButton");
 const heardBox = document.getElementById("heardBox");
 const heardText = document.getElementById("heardText");

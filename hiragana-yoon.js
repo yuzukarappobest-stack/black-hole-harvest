0fed75ffcfad2495b74b69ef4bfc4a789efcdfc5
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-21 18:00:04 +0900

    Route hiragana-yoon rewards through game menu

--- hiragana-yoon.js
+++ hiragana-yoon.js
@@ -533,7 +533,7 @@ function showFeedback(text, type) {
 
 function showComplete() {
   issueRewardToken();
-  completePanel.classList.remove("hidden");
+  window.location.replace("game-menu.html");
 }
 
 function resetLesson() {

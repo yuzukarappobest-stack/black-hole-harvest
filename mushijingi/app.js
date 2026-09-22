974895298baf8dee04e027449304043ce3ef8290
yuzukarappobest-stack <yuzu.karappo.best@gmail.com>
2026-09-22 13:37:43 +0900

    Use third booster random deck for CPU

--- mushijingi/app.js
+++ mushijingi/app.js
@@ -371,7 +371,7 @@
   }
 
   async function startGame(deckKey, firstSide){
-    const cpuKey=(deckKey==='random1'||deckKey==='random2')?deckKey:(deckKey==='kabuto'?'mantis':'kabuto');
+    const cpuKey=(deckKey==='random1'||deckKey==='random2'||deckKey==='random3')?deckKey:(deckKey==='kabuto'?'mantis':'kabuto');
     uidCounter=1;
     state={player:makeSide(deckKey,false),cpu:makeSide(cpuKey,true),turn:firstSide,turnSeq:1,turnNo:1,phase:'draw',over:false,winner:null,log:[],chain:null,busy:false};
     startScreen.classList.add('hidden'); gameScreen.classList.remove('hidden');
